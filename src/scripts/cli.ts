26a20527324d51800281c2c1045715b8dcae9dac
JBurbanoDrVet <jhony.burbano@drvet.com.co>
2021-03-10 20:09:00 -0500

    response parse arguments

--- src/scripts/cli.ts
+++ src/scripts/cli.ts
@@ -3,14 +3,25 @@ import { Options } from '../typings/index';
 import { ARGUMENTS, ALIAS } from '../utils/constants/index';
 
 function parseArgumentsIntoOptions(rawArgs: string[]): Options {
-    // console.log(rawArgs);
-    // return {
-    //     skipPrompts: '';
-    //     git: boolean;
-    //     template: string;
-    //     runInstall: boolean;
-    //     targetDir: string;
-    // }
+    const args = arg(
+        {
+            [ARGUMENTS.GIT]: Boolean,
+            [ARGUMENTS.YES]: Boolean,
+            [ARGUMENTS.INSTALL]: Boolean,
+            [ALIAS.GIT]: ARGUMENTS.GIT,
+            [ALIAS.YES]: ARGUMENTS.YES,
+            [ALIAS.INSTALL]: ARGUMENTS.INSTALL,
+        }, 
+        {argv: rawArgs.slice(2)}
+    );
+
+    return {
+        skipPrompts: !!args[ARGUMENTS.YES],
+        git: !!args[ARGUMENTS.GIT],
+        template: args._[0],
+        runInstall: !!args[ARGUMENTS.INSTALL];
+        targetDir: process.cwd();
+    }
 }
 
 export async function cli(args: string[]){
